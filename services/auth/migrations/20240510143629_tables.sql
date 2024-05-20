696b81a61631bc1fbee04d503c0c704ec1de5924
Yousof Mersal <yousof777@gmail.com>
2024-05-20 17:29:44 +0200

    make email unique

--- services/auth/migrations/20240510143629_tables.sql
+++ services/auth/migrations/20240510143629_tables.sql
@@ -3,7 +3,7 @@ create table if not exists users (
   id bigserial primary key,
   username varchar(255) not null unique,
   password varchar(255),
-  email varchar(255),
+  email varchar(255) unique,
   access_token varchar(255),
   created_at timestamp default current_timestamp
 );
