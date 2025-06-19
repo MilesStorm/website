ef48493124e14bae48e01e8dda8540ad1de3f295
Yousof Mersal <yousof777@gmail.com>
2025-06-19 07:34:29 +0000

    correct name

--- services/frontend/src/pages/landing.rs
+++ services/frontend/src/pages/landing.rs
@@ -9,7 +9,7 @@ pub fn Landing() -> Element {
             div {
                 class: "container mx-auto mt-10",
                 p { class: "text", "Login to see your profile"}
-                p { class: "text", "if you're page is blank, contact yousof to get features added"}
+                p { class: "text", "if you're page is blank, contact Miles to get features added"}
                 p { class: "text", "As this is hosted on my own server i have to limit the people who can access the different features for now"}
                 p { class: "text", "but when a feature can take many users i will make it publically available"}
             }
