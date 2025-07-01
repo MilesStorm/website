d308226da69b524d10083c36459dae3b10a04483
Miles Storm <milesrstorm@gmail.com>
2025-07-02 00:24:04 +0200

    correct redirect url

--- services/auth/src/auth/user.rs
+++ services/auth/src/auth/user.rs
@@ -192,7 +192,7 @@ impl Backend {
             RedirectUrl::new(String::from(if cfg!(debug_assertions) {
                 "http://localhost:8080/api/login/google/callback"
             } else {
-                "https://MILESSTORM.COM/API/LOGin/google/callback"
+                "https://milesstorm.com/api/login/google/callback"
             }))
             .expect("invalid redirect uri"),
         );
