1c4e71c933d07612f167c2c582c4ac553d4cf9eb
Miles Storm <milesrstorm@gmail.com>
2025-07-15 08:24:06 +0000

    remove traces of old name

--- services/auth/migrations/20240510143629_tables.sql
+++ services/auth/migrations/20240510143629_tables.sql
@@ -7,5 +7,3 @@ create table if not exists users (
   access_token varchar(255),
   created_at timestamp default current_timestamp
 );
-
--- insert into users (id, username, email, password) values (1, 'yousof', 'yousof777@gmail.com', '$argon2i$v=19$m=16,t=2,p=1$WXB3ZVNTM2dOSmJIYXNlMw$tDUgH5HWPTWTn+U4qMeNEQ');
